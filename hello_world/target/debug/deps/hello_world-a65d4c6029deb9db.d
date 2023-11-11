ec95300bd78dfafcd518a74ba90691d70fc799dd
umareh <umareh@gmail.com>
2023-11-11 13:27:10 -0600

    commiting hello world

---
+++ hello_world/target/debug/deps/hello_world-a65d4c6029deb9db.d
@@ -0,0 +1,5 @@
+/Users/omumare/rust_project/rust_project/hello_world/target/debug/deps/hello_world-a65d4c6029deb9db: src/main.rs
+
+/Users/omumare/rust_project/rust_project/hello_world/target/debug/deps/hello_world-a65d4c6029deb9db.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/omumare/rust_project/rust_project/hello_world/target/debug/hello_world: /Users/omumare/rust_project/rust_project/hello_world/src/main.rs
